22c4e6c1b75123d7ffac345c38e98bd87647ed66
Alon Greenstein <alon996@gmail.com>
2026-05-01 14:44:37 +0300

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        nums = {}
+        for x in range(1,10):
+            nums[str(x)] = False
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                elif nums[board[i][j]]:
+                    return False
+                nums[board[i][j]] = True
+            for k in nums:
+                nums[k] = False
+        
+        for i in range(9):
+            for j in range(9):
+                if board[j][i] == ".":
+                    continue
+                elif nums[board[j][i]]:
+                    return False
+                nums[board[j][i]] = True
+            for k in nums:
+                nums[k] = False
+        
+        for a in range(3):
+            for b in range(3):
+                for i in range(3*a, 3*a+3):
+                    for j in range(3*b,3*b+3):
+                        if board[i][j] == ".":
+                            continue
+                        elif nums[board[i][j]]:
+                            return False
+                        nums[board[i][j]] = True
+                for k in nums:
+                    nums[k] = False
+
+        return True

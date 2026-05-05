3d1eb0299b58f04364bcced75699757d19f67119
Alon Greenstein <alon996@gmail.com>
2026-05-05 13:17:49 +0300

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = "".join(c.lower() for c in s if c.isalnum())
+        
+        if len(clean) <= 1:
+            return True
+
+        print(clean)
+
+        start = 0
+        end = len(clean) - 1
+
+        while clean[start] == clean[end]:
+            start += 1
+            end -= 1
+            if start > end:
+                return True
+
+        return False

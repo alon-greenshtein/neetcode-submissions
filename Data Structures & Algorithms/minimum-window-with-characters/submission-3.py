cdd73e1a9e00ac3e750608e8a91035cbbf1ad24c
Alon Greenstein <alon996@gmail.com>
2026-06-10 15:44:56 +0300

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,37 @@
+from collections import Counter, defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        ls = len(s)
+        lt = len(t)
+      
+        l = 0
+        r = 0
+        cs = Counter()
+        ct = Counter(t)
+      
+        sw = "a" * (ls + 1)
+        w = sw
+        while r <= ls:
+            while ct <= cs:
+                w = s[l:r]
+                cs[s[l]] -= 1
+                if cs[s[l]] == 0:
+                    del cs[s[l]]
+                l += 1
+            if len(sw) > len(w):
+                sw = w
+            if r == ls:
+                break
+            cs[s[r]] += 1    
+            r += 1
+            
+        
+        if sw == ("a" * (ls + 1)):
+            return ""
+        return sw
+            
+
+            
+           
+
+        
